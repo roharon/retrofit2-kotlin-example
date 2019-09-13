24411a9d2c9f791b69af315370545921d7ff8410
roharon <roharon@studentpartner.com>
2019-09-13 23:47:02 +0900

    add: Retrofit2 API CALL

---
+++ app/src/main/java/com/example/retrofit2_kotlin/Retrofit2/WeatherService.kt
@@ -0,0 +1,10 @@
+package com.example.retrofit2_kotlin.Retrofit2
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService{
+    @GET("data/2.5/weather?")
+    fun getCurrentWeatherData(@Query("lat") lat : String, @Query("lon") lon : String, @Query("APPID") APPID : String): Call<WeatherResponse>
+}
